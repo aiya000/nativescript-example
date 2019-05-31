4ae63c468d410a201a5c45cf1fa1f94daa1db3b1
aiya000 <aiya000.develop@gmail.com>
2019-05-31 11:21:33 +0900

    :+1: Init project

---
+++ platforms/android/app/src/main/assets/app/bundle.js
@@ -0,0 +1,330 @@
+module.exports =
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["bundle"],{
+
+/***/ "./ sync ^\\.\\/app\\.(css|scss|less|sass)$":
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./app.css": "./app.css"
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	var id = map[req];
+	if(!(id + 1)) { // check for number or string
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return id;
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = "./ sync ^\\.\\/app\\.(css|scss|less|sass)$";
+
+/***/ }),
+
+/***/ "./ sync recursive (root|page)\\.(xml|css|js|ts|scss)$":
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./app-root.xml": "./app-root.xml",
+	"./main-page.ts": "./main-page.ts",
+	"./main-page.xml": "./main-page.xml"
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	var id = map[req];
+	if(!(id + 1)) { // check for number or string
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return id;
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = "./ sync recursive (root|page)\\.(xml|css|js|ts|scss)$";
+
+/***/ }),
+
+/***/ "./app-root.xml":
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(global) {
+module.exports = "<Frame defaultPage=\"main-page\">\n</Frame>\n";
+    if (true) {
+        module.hot.accept();
+        module.hot.dispose(() => {
+            global.hmrRefresh({ type: 'markup', path: './app-root.xml' });
+        })
+    }
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__("../node_modules/webpack/buildin/global.js")))
+
+/***/ }),
+
+/***/ "./app.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(global) {exports = module.exports = __webpack_require__("../node_modules/css-loader/dist/runtime/api.js")(false);
+// Imports
+exports.i(__webpack_require__("../node_modules/css-loader/dist/cjs.js?!../node_modules/nativescript-theme-core/css/core.light.css"), "");
+
+// Module
+exports.push([module.i, "/*\nIn NativeScript, the app.css file is where you place CSS rules that\nyou would like to apply to your entire application. Check out\nhttp://docs.nativescript.org/ui/styling for a full list of the CSS\nselectors and properties you can use to style UI components.\n\n/*\nIn many cases you may want to use the NativeScript core theme instead\nof writing your own CSS rules. For a full list of class names in the theme\nrefer to http://docs.nativescript.org/ui/theme. \nThe imported CSS rules must precede all other types of rules.\n*/\n\n/*\nThe following CSS rule changes the font size of all UI\ncomponents that have the btn class name.\n*/\n.btn {\n    font-size: 18;\n}\n", ""]);
+
+;
+    if (true) {
+        module.hot.accept();
+        module.hot.dispose(() => {
+            global.hmrRefresh({ type: 'style', path: './app.css' });
+        })
+    }
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__("../node_modules/webpack/buildin/global.js")))
+
+/***/ }),
+
+/***/ "./app.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(global) {/* harmony import */ var tns_core_modules_application__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("tns-core-modules/application");
+/* harmony import */ var tns_core_modules_application__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(tns_core_modules_application__WEBPACK_IMPORTED_MODULE_0__);
+
+        let applicationCheckPlatform = __webpack_require__("tns-core-modules/application");
+        if (applicationCheckPlatform.android && !global["__snapshot"]) {
+            __webpack_require__("tns-core-modules/ui/frame");
+__webpack_require__("tns-core-modules/ui/frame/activity");
+        }
+
+        
+            __webpack_require__("../node_modules/nativescript-dev-webpack/load-application-css-regular.js")();
+            
+            
+        if (true) {
+            const hmrUpdate = __webpack_require__("../node_modules/nativescript-dev-webpack/hmr/index.js").hmrUpdate;
+            global.__initialHmrUpdate = true;
+            global.__hmrSyncBackup = global.__onLiveSync;
+
+            global.__onLiveSync = function () {
+                hmrUpdate();
+            };
+
+            global.hmrRefresh = function({ type, path } = {}) {
+                if (global.__initialHmrUpdate) {
+                    return;
+                }
+
+                setTimeout(() => {
+                    global.__hmrSyncBackup({ type, path });
+                });
+            };
+
+            hmrUpdate().then(() => {
+                global.__initialHmrUpdate = false;
+            })
+        }
+        
+            const context = __webpack_require__("./ sync recursive (root|page)\\.(xml|css|js|ts|scss)$");
+            global.registerWebpackModules(context);
+            
+        __webpack_require__("tns-core-modules/bundle-entry-points");
+        /*
+In NativeScript, the app.ts file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
+
+tns_core_modules_application__WEBPACK_IMPORTED_MODULE_0__["run"]({ moduleName: "app-root" });
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
+
+    
+        
+        
+    
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__("../node_modules/webpack/buildin/global.js")))
+
+/***/ }),
+
+/***/ "./main-page.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(global) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "navigatingTo", function() { return navigatingTo; });
+/* harmony import */ var _main_view_model__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./main-view-model.ts");
+/*
+In NativeScript, a file with the same name as an XML file is known as
+a code-behind file. The code-behind is a great place to place your view
+logic, and to set up your page’s data binding.
+*/
+
+// Event handler for Page "navigatingTo" event attached in main-page.xml
+function navigatingTo(args) {
+    /*
+    This gets a reference this page’s <Page> UI component. You can
+    view the API reference of the Page to see what’s available at
+    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
+    */
+    var page = args.object;
+    /*
+    A page’s bindingContext is an object that should be used to perform
+    data binding between XML markup and TypeScript code. Properties
+    on the bindingContext can be accessed using the {{ }} syntax in XML.
+    In this example, the {{ message }} and {{ onTap }} bindings are resolved
+    against the object returned by createViewModel().
+
+    You can learn more about data binding in NativeScript at
+    https://docs.nativescript.org/core-concepts/data-binding.
+    */
+    page.bindingContext = new _main_view_model__WEBPACK_IMPORTED_MODULE_0__["HelloWorldModel"]();
+}
+;
+    if (true) {
+        module.hot.accept();
+        module.hot.dispose(() => {
+            global.hmrRefresh({ type: 'script', path: './main-page.ts' });
+        })
+    }
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__("../node_modules/webpack/buildin/global.js")))
+
+/***/ }),
+
+/***/ "./main-page.xml":
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(global) {
+module.exports = "<!--\nThe markup in NativeScript apps contains a series of user interface components, each\nof which NativeScript renders with a platform-specific iOS or Android native control.\nYou can find a full list of user interface components you can use in your app at\nhttps://docs.nativescript.org/ui/components.\n-->\n<Page xmlns=\"http://schemas.nativescript.org/tns.xsd\" navigatingTo=\"navigatingTo\" class=\"page\">\n    <!--\n    The ActionBar is the NativeScript common abstraction over the Android ActionBar and iOS NavigationBar.\n    http://docs.nativescript.org/ui/action-bar\n    -->\n    <Page.actionBar>\n        <ActionBar title=\"My App\" icon=\"\" class=\"action-bar\">\n        </ActionBar>\n    </Page.actionBar>\n    <!--\n    The StackLayout stacks UI components on the screen—either vertically or horizontally.\n    In this case, the StackLayout does vertical stacking; you can change the stacking to\n    horizontal by applying a orientation=\"horizontal\" attribute to the <StackLayout> element.\n    You can learn more about NativeScript layouts at\n    https://docs.nativescript.org/ui/layout-containers.\n\n    These components make use of several CSS class names that are part of the NativeScript\n    core theme, such as p-20, btn, h2, and text-center. You can view a full list of the\n    class names available for styling your app at https://docs.nativescript.org/ui/theme.\n    -->\n    <StackLayout class=\"p-20\">\n        <Label text=\"Tap the button\" class=\"h1 text-center\"/>\n        <Button text=\"TAP\" tap=\"{{ onTap }}\" class=\"btn btn-primary btn-active\"/>\n        <Label text=\"{{ message }}\" class=\"h2 text-center\" textWrap=\"true\"/>\n    </StackLayout>\n</Page>\n";
+    if (true) {
+        module.hot.accept();
+        module.hot.dispose(() => {
+            global.hmrRefresh({ type: 'markup', path: './main-page.xml' });
+        })
+    }
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__("../node_modules/webpack/buildin/global.js")))
+
+/***/ }),
+
+/***/ "./main-view-model.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HelloWorldModel", function() { return HelloWorldModel; });
+/* harmony import */ var tns_core_modules_data_observable__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("tns-core-modules/data/observable");
+/* harmony import */ var tns_core_modules_data_observable__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(tns_core_modules_data_observable__WEBPACK_IMPORTED_MODULE_0__);
+
+var HelloWorldModel = /** @class */ (function (_super) {
+    __extends(HelloWorldModel, _super);
+    function HelloWorldModel() {
+        var _this = _super.call(this) || this;
+        // Initialize default values.
+        _this._counter = 42;
+        _this.updateMessage();
+        return _this;
+    }
+    Object.defineProperty(HelloWorldModel.prototype, "message", {
+        get: function () {
+            return this._message;
+        },
+        set: function (value) {
+            if (this._message !== value) {
+                this._message = value;
+                this.notifyPropertyChange("message", value);
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    HelloWorldModel.prototype.onTap = function () {
+        this._counter--;
+        this.updateMessage();
+    };
+    HelloWorldModel.prototype.updateMessage = function () {
+        if (this._counter <= 0) {
+            this.message = "Hoorraaay! You unlocked the NativeScript clicker achievement!";
+        }
+        else {
+            this.message = this._counter + " taps left";
+        }
+    };
+    return HelloWorldModel;
+}(tns_core_modules_data_observable__WEBPACK_IMPORTED_MODULE_0__["Observable"]));
+
+
+
+/***/ }),
+
+/***/ "tns-core-modules/application":
+/***/ (function(module, exports) {
+
+module.exports = require("tns-core-modules/application");
+
+/***/ }),
+
+/***/ "tns-core-modules/bundle-entry-points":
+/***/ (function(module, exports) {
+
+module.exports = require("tns-core-modules/bundle-entry-points");
+
+/***/ }),
+
+/***/ "tns-core-modules/data/observable":
+/***/ (function(module, exports) {
+
+module.exports = require("tns-core-modules/data/observable");
+
+/***/ }),
+
+/***/ "tns-core-modules/file-system":
+/***/ (function(module, exports) {
+
+module.exports = require("tns-core-modules/file-system");
+
+/***/ }),
+
+/***/ "tns-core-modules/ui/frame":
+/***/ (function(module, exports) {
+
+module.exports = require("tns-core-modules/ui/frame");
+
+/***/ }),
+
+/***/ "tns-core-modules/ui/frame/activity":
+/***/ (function(module, exports) {
+
+module.exports = require("tns-core-modules/ui/frame/activity");
+
+/***/ }),
+
+/***/ "tns-core-modules/ui/styling/style-scope":
+/***/ (function(module, exports) {
+
+module.exports = require("tns-core-modules/ui/styling/style-scope");
+
+/***/ })
+
+},[["./app.ts","runtime","vendor"]]]);
